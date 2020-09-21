9e223e6fcbd98288f32da03217b31381952b3df7
Richard David <48126051+itsarcanedave@users.noreply.github.com>
2020-09-22 00:30:18 +0700

    Updated to match knock-knock constraints

--- Client.py
+++ Client.py
@@ -1,11 +1,40 @@
 import socket
 import ipaddress
 
-ip = input ("Enter server IP address: ")
+name =  input ("Please enter your name: ")
+print ("Hello", name, "!")
+local = socket.gethostbyname(socket.gethostname())
+print ("Your client IP address is", local)
+ip = input ("Please enter server IP address: ")
 ipaddress.ip_address(ip)
-port = int(input ("Enter port: "))
-while(True):
-    messageString = input("Enter a message: ")
-    message = str.encode(messageString)
-    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    clientSock.sendto(message, (ip, port))
+port = int(input ("Please enter port: "))
+
+counter = 1
+while (counter <= 3):
+    if counter <=2:
+        messageString = input("Enter a message: ")
+        separator = str(": ")
+        username = str.encode(name)
+        separatorSend = str.encode(separator)
+        message = str.encode(messageString)
+        sendMessage = username + separatorSend + message
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        clientSocket.sendto(sendMessage, (ip, port))
+        data, server = clientSocket.recvfrom(4096)
+        dataString = data.decode()
+        print (dataString)
+        counter = counter + 1
+    elif counter == 3:
+        messageString = input("Enter a message: ")
+        separator = str(": ")
+        username = str.encode(name)
+        separatorSend = str.encode(separator)
+        message = str.encode(messageString)
+        sendMessage = username + separatorSend + message
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        clientSocket.sendto(sendMessage, (ip, port))
+        break
+
+print ("Connection closed!")     
+done = input("Press any key to exit!")
+        
--- Server.py
+++ Server.py
@@ -3,12 +3,40 @@ import socket
 ip = socket.gethostbyname(socket.gethostname())
 print ("Server is online at", ip)
 port = int(input ("Enter port: "))
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverSock.bind((ip, port))
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSocket.bind((ip, port))
 print ("Listening on port", port)
 
-while True:
-    data, addr = serverSock.recvfrom(1024)
-    dataString = data.decode()
-    print ("Message: ", dataString)
+
+counter = 1
+while (counter <= 3):
+    if counter == 1:
+        data, address = serverSocket.recvfrom(1024)
+        dataString = data.decode()
+        print (dataString)
+        firstMessage = str("Who's there?")
+        firstReply = str.encode(firstMessage)
+        reply = serverSocket.sendto(firstReply, address)
+        counter = counter + 1
+    elif counter == 2:
+        data, address = serverSocket.recvfrom(1024)
+        dataString = data.decode()
+        print (dataString)
+        secondMessage = dataString.partition(":")[2]
+        strippedMessage = secondMessage.lstrip()
+        strippedEncode = str.encode(strippedMessage)
+        who = str(" who?")
+        whoMessage = str.encode(who)
+        secondReply = strippedEncode + whoMessage
+        reply = serverSocket.sendto(secondReply, address)
+        counter = counter + 1
+    elif counter == 3:
+        data, address = serverSocket.recvfrom(1024)
+        dataString = data.decode()
+        print (dataString)
+        break
+
+print ("Connection closed!")     
+done = input("Press any key to exit!")
+      
   
